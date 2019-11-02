b1773b0c864eae353a3727c28edbd1d0e0df4abb
Joshua Steele <joshukraine@gmail.com>
2019-11-02 13:49:58 +0200

    Update handler.js.
    * Reference: https://dev.to/adnanrahic/building-a-serverless-contact-form-with-aws-lambda-and-aws-ses-4jm0

--- handler.js
+++ handler.js
@@ -1,18 +1,66 @@
-'use strict';
+const aws = require('aws-sdk');
 
-module.exports.contactMailer = async event => {
+const ses = new aws.SES();
+const myEmail = process.env.EMAIL;
+const myDomain = process.env.DOMAIN;
+
+function generateResponse(code, payload) {
   return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
+    statusCode: code,
+    headers: {
+      'Access-Control-Allow-Origin': myDomain,
+      'Access-Control-Allow-Headers': 'x-requested-with',
+      'Access-Control-Allow-Credentials': true,
+    },
+    body: JSON.stringify(payload),
   };
+}
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+function generateError(code, err) {
+  console.log(err);
+  return {
+    statusCode: code,
+    headers: {
+      'Access-Control-Allow-Origin': myDomain,
+      'Access-Control-Allow-Headers': 'x-requested-with',
+      'Access-Control-Allow-Credentials': true,
+    },
+    body: JSON.stringify(err.message),
+  };
+}
+
+function generateEmailParams(body) {
+  const { email, name, content } = JSON.parse(body);
+  console.log(email, name, content);
+  if (!(email && name && content)) {
+    throw new Error('Missing parameters! Make sure to add parameters \'email\', \'name\', \'content\'.');
+  }
+
+  return {
+    Source: myEmail,
+    Destination: { ToAddresses: [myEmail] },
+    ReplyToAddresses: [email],
+    Message: {
+      Body: {
+        Text: {
+          Charset: 'UTF-8',
+          Data: `Message sent from email ${email} by ${name} \nContent: ${content}`,
+        },
+      },
+      Subject: {
+        Charset: 'UTF-8',
+        Data: `You received a message from ${myDomain}!`,
+      },
+    },
+  };
+}
+
+module.exports.send = async (event) => {
+  try {
+    const emailParams = generateEmailParams(event.body);
+    const data = await ses.sendEmail(emailParams).promise();
+    return generateResponse(200, data);
+  } catch (err) {
+    return generateError(500, err);
+  }
 };
